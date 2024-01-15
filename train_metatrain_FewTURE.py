801bb4da8b82e9083f5c8cb238a76f0cc708b76c
DRGreat <297772967@qq.com>
2024-01-15 22:09:01 +0800

    m

--- train_metatrain_FewTURE.py
+++ train_metatrain_FewTURE.py
@@ -388,7 +388,7 @@ class NewPatchFSL(nn.Module):
         self.hyperpixel_ids = hyperpixel_ids
 
         self.feature_size = 14
-        self.feature_proj_dim = 3
+        self.feature_proj_dim = 4
         self.decoder_embed_dim = self.feature_size ** 2 + self.feature_proj_dim
 
         self.proj = nn.ModuleList([
